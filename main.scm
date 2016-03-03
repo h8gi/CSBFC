878a893287bac16fe7d04b22c12fa95c22c86481
hiroki yagi <hiro.yagi0245@gmail.com>
2016-03-03 21:29:51 +0900

    skip illegal char

--- main.scm
+++ main.scm
@@ -44,8 +44,17 @@
     [(#\+ #\- #\< #\>) ch]
     [else #f]))
 
+(define (legal? ch)
+  (case ch
+    [(#\+ #\- #\< #\> #\, #\. #\[ #\]) ch]
+    [else #f]))
+
+(define (skip-read-char)
+  (cond [(legal? (read-char)) => identity]
+        [else (skip-read-char)]))
+
 (define (compress)
-  (let loop ([ch (read-char)]
+  (let loop ([ch (skip-read-char)]
              [run? #f]
              [count 0])
     (cond [(eof-object? ch)
@@ -54,20 +63,20 @@
                (display (convert-char ch)))]
           [run?                         ; 今まで連続していた
            (cond [(char=? run? ch)      ; まだまだ連続
-                  (loop (read-char) run? (add1 count))]
+                  (loop (skip-read-char) run? (add1 count))]
                  [(compressable? ch)    ; 連続の起点
                   (display (expand-char run? count))
-                  (loop (read-char) ch 1)]
+                  (loop (skip-read-char) ch 1)]
                  [else                  ; 普通に処理
                   (display (expand-char run? count)) ; 今までの
                   (display (convert-char ch)) ; 今の
-                  (loop (read-char) #f 0)])]
+                  (loop (skip-read-char) #f 0)])]
           [else
            (cond [(compressable? ch)
-                  (loop (read-char) ch 1)]
+                  (loop (skip-read-char) ch 1)]
                  [else
                   (display (convert-char ch))
-                  (loop (read-char) #f 0)])])))
+                  (loop (skip-read-char) #f 0)])])))
 
 (define (bf-compile)
   ;; (let ([ch (read-char)])
